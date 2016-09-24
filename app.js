0c3d3ae53b611f4af936ff09259f93ee28ce42d6
David Holbrook <david@davidholbrook.info>
2016-09-24 00:15:12 -0500

    Round 2
    Added some unsplashed goodness as well took out the title (since it was dumb).
    
    Signed-off-by: David Holbrook <david@davidholbrook.info>

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
-
-
-
-
-
-
+  var rand = Math.round(Math.random()*100)
+  console.log(rand)
+  $.getJSON("https://unsplash.it/list", function(data){
+    $('body').css({
+      'background':'url(https://unsplash.it/' + data[rand].filename+')',
+      'background-size':'cover'
+    });
+  }).fail(function(){
+    console.error(data);
+  });
 });
