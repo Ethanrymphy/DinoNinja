f7d972fc858253c0e969c240027562cb9507e021
Ethanrymphy <erymph@mcad.edu>
2016-09-26 13:20:36 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -8,5 +8,26 @@ $(document).ready(function() {
     });
   }).fail(function(){
     console.error(data);
+
+    $(".dino").hover(function(){
+        $(this).css("background-color", "yellow");
+        }, function(){
+        $(this).css("background-color", "pink");
+    });
+
+
   });
+});
+
+myAudio = new Audio('dinoninja.mp3');
+myAudio.addEventListener('ended', function() {
+  this.currentTime = 0;
+  this.play();
+}, false);
+myAudio.play();
+
+$(document).ready( function() {
+$('..alternate1').click(function(){
+   $('.alternate1').append('<embed id="embed_player" src="audio.wav" autostart="true" hidden="true"></embed>');
+});
 });
